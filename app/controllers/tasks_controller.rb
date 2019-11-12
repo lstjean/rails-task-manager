f52632012a117aba232b72b19a61cbc4488311f4
Lauren St Jean <lauren.stjean@gmail.com>
2019-11-12 16:54:18 +0100

    Almost everything works, but delete and making sure you can readh the value of the checkbox

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,48 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    set_task
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to tasks_path
+  end
+
+  def update
+    if @task.update(task_params)
+      redirect_to tasks_path
+    else
+      render :edit
+    end
+  end
+
+  def edit
+  end
+
+  def destroy
+    set_task
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+task = Task.new(title: "Cooking", details: "make something for dinner")
+task2 = Task.new(title: "Bathroom", details: "Clean the bathtub")
+task3 = Task.new(title: "Feed dog", details: "make it yummy")
+
+task.save
+task2.save
+task3.save
